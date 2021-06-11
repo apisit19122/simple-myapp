4cc5e73ea93f5701deec922192ae055d1b91e0ab
ori-dev <ori-dev@MacBook-Pro-khxng-ori-dev.local>
2021-06-12 06:52:53 +0700

    update

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/oat", (req, res) => {
   res.send("im OaTT3");
 });
 
+app.get("/home", (req, res) => {
+  res.send("This is home page.");
+});
+
 app.listen(9000, () => {
   console.log("http://127.0.0.1:9000");
 });
